72e93de598ed9ee19e76fe7b2642c8560a13fc54
school <school@A45PC16>
2025-02-25 09:56:12 +0200

    Додав меню

--- okokokokoko/Program.cs
+++ okokokokoko/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace okokokokoko
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Darova eta Класний журнал");
+Console.WriteLine("\nМеню:");
+Console.WriteLine("1. Додати учня");
+Console.WriteLine("2. Редагувати учня");
+Console.WriteLine("3. Видалити учня");
+Console.WriteLine("4. Додати предмет");
+Console.WriteLine("5. Редагувати предмет");
+Console.WriteLine("6. Видалити предмет");
+Console.WriteLine("7. Внести оцінку");
+Console.WriteLine("8. Редагувати оцінку");
+Console.WriteLine("9. Обчислити середній бал");
+Console.WriteLine("10. Зберегти дані");
+Console.WriteLine("11. Вийти");
