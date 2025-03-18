e2e2b0d08cb6be38481d9f80ffe85f23a9e454ed
school <school@A45PC10>
2025-03-18 10:01:39 +0200

    konuj9iohj

--- okokokokoko/Program.cs
+++ okokokokoko/Program.cs
@@ -1,18 +1,50 @@
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.WriteLine("Darova eta Класний журнал");
-Console.WriteLine("\nМеню:");
-Console.WriteLine("1. Додати учня");
-Console.WriteLine("2. Редагувати учня");
-Console.WriteLine("3. Видалити учня");
-Console.WriteLine("4. Додати предмет");
-Console.WriteLine("5. Редагувати предмет");
-Console.WriteLine("6. Видалити предмет");
-Console.WriteLine("7. Внести оцінку");
-Console.WriteLine("8. Редагувати оцінку");
-Console.WriteLine("9. Обчислити середній бал");
-Console.WriteLine("10. Зберегти дані");
-Console.WriteLine("11. Вийти");
+Console.WriteLine("\nMenu:");
+Console.WriteLine("1. Add student");
+Console.WriteLine("2. Edit student");
+Console.WriteLine("3. Delete student");
+Console.WriteLine("4. Add subject");
+Console.WriteLine("5. Edit subject");
+Console.WriteLine("6. Delete subject");
+Console.WriteLine("7. Enter grade");
+Console.WriteLine("8. Edit grade");
+Console.WriteLine("9. Calculate GPA");
+Console.WriteLine("10. Save data");
+Console.WriteLine("11. Exit");
+
+Student st = new();
+
+while (true)
+{
+    Console.Write("Your choice: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Console.Write("Enter Student FirstName: ");
+            st.FirstName = Console.ReadLine();
+            Console.Write("Enter Student Age: ");
+            st.Age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Student Last name: ");
+            st.LastName = Console.ReadLine();
+            Console.Write("Enter Student Class: ");
+            st.Class = Console.ReadLine();
+           
+            break;
+        case 2:
+            Console.WriteLine("------- Student ---------");
+            Console.WriteLine($"Name: {st.FirstName}");
+            Console.WriteLine($"Age: {st.Age}");
+            Console.WriteLine($"Class: {st.Class}");
+            break;
+    }
+
+
+
+
 public class Student
 {
     public string FirstName { get; set; }
