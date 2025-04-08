c40ab3fff3bc06e36f8ef393076031f44497e3dc
EugenSigmaTailerDerden <kzena508@gmail.com>
2025-04-08 09:47:15 +0300

    Update Program.csвава

--- okokokokoko/Program.cs
+++ okokokokoko/Program.cs
@@ -14,8 +14,8 @@ Console.WriteLine("9. Calculate GPA");
 Console.WriteLine("10. Save data");
 Console.WriteLine("11. Exit");
 
-Student st = new();
-Subject sub = new();
+List<Student> students = new();
+List<Subject> Subjects = new();
 
 while (true)
 {
@@ -24,7 +24,12 @@ while (true)
 
     switch (choice)
     {
+        case 11:
+            Environment.Exit(0);
+            break;
+
         case 1:
+            Student st = new Student();
             Console.Write("Enter Student FirstName: ");
             st.FirstName = Console.ReadLine();
             Console.Write("Enter Student Age: ");
@@ -33,18 +38,48 @@ while (true)
             st.LastName = Console.ReadLine();
             Console.Write("Enter Student Class: ");
             st.Class = Console.ReadLine();
-
+            students.Add(st);
             break;
+
         case 2:
-            Console.WriteLine("------- Student ---------");
-            Console.WriteLine($"Name: {st.FirstName}");
-            Console.WriteLine($"Age: {st.Age}");
-            Console.WriteLine($"Class: {st.Class}");
+            if (students.Count == 0)
+            {
+                Console.WriteLine("No students available!");
+                break;
+            }
+            Console.WriteLine("------- Students List ---------");
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine($"[{i}] Name: {students[i].FirstName} {students[i].LastName}");
+                Console.WriteLine($"   Age: {students[i].Age}");
+                Console.WriteLine($"   Class: {students[i].Class}");
+                Console.WriteLine("----------------------------");
+            }
+            break;
+
+        case 3:
+            if (students.Count == 0)
+            {
+                Console.WriteLine("No students to delete!");
+                break;
+            }
+            for (int i = 0; i < students.Count; ++i)
+                Console.WriteLine($"[{i}] Student: {students[i].FirstName} {students[i].LastName}");
+
+            Console.Write("Enter number to delete: ");
+            int numToDelete = Convert.ToInt32(Console.ReadLine());
+
+            if (numToDelete < 0 || numToDelete >= students.Count)
+            {
+                Console.WriteLine("Number out of range!");
+                break;
+            }
+
+            students.RemoveAt(numToDelete);
+            Console.WriteLine("Student deleted successfully!");
             break;
     }
-
 }
-
 
 public class Student
 {
@@ -58,6 +93,6 @@ public class Subject
 {
     public string Name { get; set; }
     public string Teacher { get; set; }
-    public string Room { get; set; } 
+    public string Room { get; set; }
     public string Textbook { get; set; }
 }
